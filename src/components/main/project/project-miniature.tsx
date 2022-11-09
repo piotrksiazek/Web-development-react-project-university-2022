db8ae241d4f726852f6425ee29c307962706ce31
piotr <piotr.ksiazek.mw@gmail.com>
2022-11-09 19:18:47 +0100

    add loading animation

--- src/components/main/project/project-miniature.tsx
+++ src/components/main/project/project-miniature.tsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { cdnBaseUrl } from '../../../constants';
+import { ClipLoader } from 'react-spinners';
 import "./project-miniature.scss";
 
 interface PrimaryProps {
     img: string;
+    onClick: () => void;
+    opacity: string;
 }
 
-const Primary = ({img} : PrimaryProps) => {
-    return <img className='img-primary' src={cdnBaseUrl + img}/>
+const Primary = ({img, onClick, opacity} : PrimaryProps) => {
+    return <img
+                onClick={onClick} 
+                style={{opacity: opacity}} 
+                className='img-primary' 
+                src={img}
+                alt=""
+            />
 }
 
 
@@ -20,23 +29,56 @@ export interface ProjectMiniatureProps{
 
 export const ProjectMiniature = ({index, primaryImage, activeIndex, setActiveIndex} : ProjectMiniatureProps) => {
     const [style, setStyle] = useState("50%")
+    const [isImgLoaded, setIsImgLoaded] = useState(false);
+    const [opacity, setOpacity] = useState("100%");
+    const [img, setImg] = useState("");
     
     const handleClick = () => {
         if(activeIndex === index){
             setStyle("50%");
             setActiveIndex(-1);
+            setOpacity("30%");
         } else if(activeIndex < 0){
             setActiveIndex(index);
             setStyle("90%");
+            setOpacity("100%");
         }
     }
 
-    return (
-        <div className={'miniature-image-container'}
-            style={{width: style, height: style}}
-            onClick={() => {handleClick()}}>
-                <Primary img={primaryImage}/>
-        </div>
-    );
-    
+    const fetchImage = async (imageUrl: string) => {
+        const res = await fetch(imageUrl);
+        setIsImgLoaded(true);
+        const imageBlob = await res.blob();
+        const imageObjectURL = URL.createObjectURL(imageBlob);
+        setImg(imageObjectURL);
+    };
+
+    useEffect(() => {
+        fetchImage(cdnBaseUrl + primaryImage);
+      }, []);
+
+    if(isImgLoaded)
+    {
+        return (
+            <div className={'miniature-image-container'}
+                style={{width: style, height: style}}
+                >
+                    {activeIndex === index ? <button className='btn'>left</button> : <div/>}
+                    <Primary 
+                        onClick={() => handleClick()}
+                        opacity={opacity} 
+                        img={img}
+                        />
+                    {activeIndex === index ? <button className='btn'>right</button> : <div/>}
+            </div>
+        );
+    } else {
+        return (
+            <div className={'loader'}>
+                <ClipLoader
+                    
+                />
+            </div>
+        )
+    }
 }
