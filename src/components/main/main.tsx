24b663baf9904d47239d92c289d2811a12751222
piotr <piotr.ksiazek.mw@gmail.com>
2022-11-17 17:13:07 +0100

    mobile scaling

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -44,14 +44,74 @@ export const Main = ({imgZIndex} : MainProps) => {
                         />
                     <ProjectMiniature 
                         index={3} 
-                        primaryImage={"prince.jpg"} 
+                        primaryImage={"wiatraki/1.jpg"} 
                         activeIndex={activeMiniature} 
                         setActiveIndex={setActiveMiniature} 
                         opacityForAllImages={opacityForAllImages} 
                         setOpacityForAllImages={setOpacityForAllImages}
-                        otherImages={["golden.jpg", "prince.jpg", "golden.jpg", "prince.jpg", "golden.jpg", "prince.jpg"]}
+                        otherImages={["wiatraki/2.jpg", "wiatraki/3.jpg"]}
                         zIndex={imgZIndex}
-                        title={"project 3"}
+                        title={"w i a t r a k"}
+                        isVertical={true}
+                        />
+                    <ProjectMiniature 
+                        index={4} 
+                        primaryImage={"pole/1.jpg"} 
+                        activeIndex={activeMiniature} 
+                        setActiveIndex={setActiveMiniature} 
+                        opacityForAllImages={opacityForAllImages} 
+                        setOpacityForAllImages={setOpacityForAllImages}
+                        otherImages={["pole/3.jpg", "pole/4.jpg", "pole/5.jpg", "pole/6.jpg"]}
+                        zIndex={imgZIndex}
+                        title={"p o l e"}
+                        isVertical={false}
+                        />
+                                        <ProjectMiniature 
+                        index={5} 
+                        primaryImage={"fuzz/1.jpg"} 
+                        activeIndex={activeMiniature} 
+                        setActiveIndex={setActiveMiniature} 
+                        opacityForAllImages={opacityForAllImages} 
+                        setOpacityForAllImages={setOpacityForAllImages}
+                        otherImages={["fuzz/2.jpg", "fuzz/3.jpg", "fuzz/4.jpg", "fuzz/5.jpg", "fuzz/6.jpg"]}
+                        zIndex={imgZIndex}
+                        title={"F U Z Z"}
+                        isVertical={false}
+                        />
+                    <ProjectMiniature 
+                        index={6} 
+                        primaryImage={"bnw/1.jpg"} 
+                        activeIndex={activeMiniature} 
+                        setActiveIndex={setActiveMiniature} 
+                        opacityForAllImages={opacityForAllImages} 
+                        setOpacityForAllImages={setOpacityForAllImages}
+                        otherImages={["bnw/2.jpg", "bnw/3.jpg", "bnw/4.jpg"]}
+                        zIndex={imgZIndex}
+                        title={"B N W"}
+                        isVertical={true}
+                        />
+                    <ProjectMiniature 
+                        index={7} 
+                        primaryImage={"wiatraki/1.jpg"} 
+                        activeIndex={activeMiniature} 
+                        setActiveIndex={setActiveMiniature} 
+                        opacityForAllImages={opacityForAllImages} 
+                        setOpacityForAllImages={setOpacityForAllImages}
+                        otherImages={["wiatraki/2.jpg", "wiatraki/3.jpg"]}
+                        zIndex={imgZIndex}
+                        title={"w i a t r a k"}
+                        isVertical={true}
+                        />
+                    <ProjectMiniature 
+                        index={8} 
+                        primaryImage={"pole/1.jpg"} 
+                        activeIndex={activeMiniature} 
+                        setActiveIndex={setActiveMiniature} 
+                        opacityForAllImages={opacityForAllImages} 
+                        setOpacityForAllImages={setOpacityForAllImages}
+                        otherImages={["pole/3.jpg", "pole/4.jpg", "pole/5.jpg", "pole/6.jpg"]}
+                        zIndex={imgZIndex}
+                        title={"p o l e"}
                         isVertical={false}
                         />
             </div>
